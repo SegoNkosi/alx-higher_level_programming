9a890daf786149b9068744eea6027d1375a30128
Lesego Nkosi <nkosilesego8@gmail.com>
2024-01-04 09:34:58 -0800

    correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from hidden_4 import *
     allfunc = dir()
-    for k in range(0, len(allfunc))
-     if allfunc[k][:2] != "__":
-         print("{:s}".format(allfunc[k]))
+    for i in range(0, len(allfunc))
+     if allfunc[i][:2] != "__":
+         print("{:s}".format(allfunc[i]))
