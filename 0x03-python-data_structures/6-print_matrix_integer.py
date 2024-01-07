fb04caf56d365c48799c3bc2d99a1b319c86a382
Lesego Nkosi <nkosilesego8@gmail.com>
2024-01-07 05:13:16 -0800

    function that prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for j in matrix:
+            for k in j:
+                print("{:d}".format(k), end=' ' if k != j[-1] else '')
+                print()
