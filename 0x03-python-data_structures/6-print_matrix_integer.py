95be12f283518e304180e388b3be04a5973b3bc4
Lesego Nkosi <nkosilesego8@gmail.com>
2024-01-07 05:17:45 -0800

    correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         for j in matrix:
             for k in j:
                 print("{:d}".format(k), end=' ' if k != j[-1] else '')
-                print()
+            print()
