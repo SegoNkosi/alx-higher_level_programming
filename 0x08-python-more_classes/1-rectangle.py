026e717c8012ed002ee7134f9ef14917449130e3
Lesego Nkosi <nkosilesego8@gmail.com>
2024-01-29 17:12:27 -0800

    all tasks

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Defines a class Rectangle.
+"""
+
+class Rectangle:
+    """Represantetion of a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initialises the rectangle
+        
+        Args:
+            width (init): The width of the new rectangle.
+            height (init): The height of the new recatngle.
+        """
+        self.height = height
+        self.width = width
+
+        @property
+        def width(self):
+            """the getter for the private instance attribute width."""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >=0")
+            self.__width = value
+
+        @property
+        def height(self):
+            """Getter of the height of the rectangle."""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            if not isinstance(value, init):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
