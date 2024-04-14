cd94411cbfbaae91bb6c3c64fd5dad7967bbb4fc
Lesego Nkosi <nkosilesego8@gmail.com>
2024-04-14 16:39:37 -0700

    task

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-"""
-The script defines a State class and
-a Base class to work with MySQLAlchemy ORM.
-"""
-import sys
-from model_state import Base, State
 
-from sqlalchemy import (create_engine)
+"""Module that lists all states from the hbtn_0e_0_usa database."""
+
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # Get MySQL credentials and state name from command-line arguments
+    # and Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+
+    # Execute the SQL query to retrieve cities in the specified state
+    query = ("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    c.execute(query)
+
+    # Fetch all rows and filter cities by the specified state
+    # and Print the cities separated by commas
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+
