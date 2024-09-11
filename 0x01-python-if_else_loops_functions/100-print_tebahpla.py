2b1bbcf1ebd1c30d206f07558fbb87ea4fd971f2
Lesego Nkosi <nkosilesego8@gmail.com>
2024-09-10 18:10:30 -0700

    task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-sys.exit(1)
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
+        diff = 0
+    else:
+        diff = 32
+    print('{}'.format(chr(i - diff)), end='')
+
