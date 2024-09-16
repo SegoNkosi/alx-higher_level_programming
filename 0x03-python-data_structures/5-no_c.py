ab89c94c9e78b87679873b61eca603c0eab31ed2
Lesego Nkosi <nkosilesego8@gmail.com>
2024-09-15 17:04:26 -0700

    a function that removes all characters c and C from a string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-no_c.py
+
+
+def no_c(my_string):
+    """Removes all characters c and C from a string."""
+    copy = [y for y in my_string if y != 'c' and y != 'C']
+    return ("".join(copy))
