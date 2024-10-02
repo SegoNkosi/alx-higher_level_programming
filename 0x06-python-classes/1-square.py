823933cc4d368594ffc176881b0405c343b35a98
Lesego Nkosi <nkosilesego8@gmail.com>
2024-10-01 17:49:03 -0700

    a class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This defines a class called Square."""
+
+
+class Square:
+    """To represent a square."""
+
+    def __init__(self, size):
+        """To initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
